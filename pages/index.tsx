ecc2cbec858fcdf61f034b8203ce5b23b9d26c7a
Pierce-44 <piercehahn.coding@gmail.com>
2022-11-03 11:41:15 +0000

    setup

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Aidlingo</title>
+        <meta name="Aidlingo" content="Aidlingo" />
+        <link rel="icon" href="/logo.svg.png" />
+      </Head>
+      <p className="text-2xl text-[#58cc02]">aidlingo</p>
+    </div>
+  );
+}
