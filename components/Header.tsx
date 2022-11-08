150486d6d4b932962ee58cde861f68a514ef5baf
Pierce-44 <piercehahn.coding@gmail.com>
2022-11-08 17:44:16 +0000

    responsive design

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,9 @@ export default function Header() {
   return (
     <div className="sticky top-0 left-0 z-50 w-full border-b-2 border-b-[#e5e5e5] bg-white">
       <div className="mx-auto flex max-w-[1328px] items-center justify-between py-3">
-        <p className="text-3xl text-[#58cc02]">aidlingo</p>
+        <p className="ml-2 text-3xl text-[#58cc02]">aidlingo</p>
         <Image
-          className="rounded-full"
+          className="mr-2 rounded-full"
           src="/avatar.jpeg"
           alt="menu"
           width={38}
