77d0c3e77d4df52b8a31993f65de1ec6ee8b937b
Pierce-44 <piercehahn.coding@gmail.com>
2022-11-08 16:20:24 +0000

    added header bg

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="sticky top-0 left-0 w-full border-b-2 border-b-[#e5e5e5]">
+    <div className="sticky top-0 left-0 z-50 w-full border-b-2 border-b-[#e5e5e5] bg-white">
       <div className="mx-auto flex max-w-[1328px] items-center justify-between py-3">
         <p className="text-3xl text-[#58cc02]">aidlingo</p>
         <Image
